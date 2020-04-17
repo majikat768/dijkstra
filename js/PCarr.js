17893970fe9a928d43ebfd601d4f592a2f878d09
Robert Goes <RobertRGoes@gmail.com>
2020-04-17 14:50:04 -0700

    TestingChanges

---
+++ js/PCarr.js
@@ -0,0 +1,41 @@
+var PseudocodeText = ["<p><span style=\"color: #0000ff;\">function<\/span> <span style=\"color: #ff0000;\">Dijkstra<\/span>(<span style=\"color: #33cccc;\">Graph<\/span>, <span style=\"color: #33cccc;\">source<\/span>):",
+"<br \/>&nbsp;&nbsp;&nbsp;create a vertex queue <span style=\"color: #33cccc;\">Q<\/span>",
+"<br \/> &nbsp;&nbsp;&nbsp;<span style=\"color: #ff00ff;\">for<\/span> each vertex <span style=\"color: #33cccc;\">v<\/span> in <span style=\"color: #33cccc;\">Graph<\/span>:",
+"<br \/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;distance[<span style=\"color: #33cccc;\">v\{<span id=\"PSid1\"><\/span>\}<\/span>] &larr; &infin;",
+"<br \/> &nbsp;&nbsp;&nbsp;<span style=\"color: #33cccc;\">Q<\/span>.<span style=\"color: #ff0000;\">enqueue<\/span>(<span style=\"color: #33cccc;\">source<\/span>)",
+"<br \/> &nbsp;&nbsp;&nbsp;<span style=\"color: #ff00ff;\">while<\/span> <span style=\"color: #33cccc;\">Q<\/span> is not empty:",
+"<br \/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style=\"color: #33cccc;\">v\{<span id=\"PSid2\"><\/span>\}<\/span> &larr; <span style=\"color: #33cccc;\">Q<\/span>.<span style=\"color: #ff0000;\">dequeue<\/span>()",
+"<br \/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style=\"color: #ff00ff;\">for<\/span> each neighbor <span style=\"color: #33cccc;\">n<\/span> of <span style=\"color: #33cccc;\">v\{<span id=\"PSid3\"><\/span>\}<\/span>:",
+"<br \/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style=\"color: #33cccc;\">dist\{<span id=\"PSid10\"><\/span>\}<\/span> &larr; distance[<span style=\"color: #33cccc;\">v\{<span id=\"PSid4\"><\/span>\}<\/span>] + <span style=\"color: #ff0000;\">length<\/span>(<span style=\"color: #33cccc;\">v\{<span id=\"PSid5\"><\/span>\}<\/span>,<span style=\"color: #33cccc;\">n\{<span id=\"PSid6\"><\/span>\}<\/span>)",
+"<br \/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style=\"color: #ff00ff;\">if<\/span> <span style=\"color: #33cccc;\">dist\{<span id=\"PSid11\"><\/span>\}<\/span> &lt; distance[<span style=\"color: #33cccc;\">n\{<span id=\"PSid7\"><\/span>\}<\/span>]:",
+"<br \/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;distance[<span style=\"color: #33cccc;\">n\{<span id=\"PSid8\"><\/span>\}<\/span>] &larr; <span style=\"color: #33cccc;\">dist\{<span id=\"PSid12\"><\/span>\}<\/span>",
+"<br \/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style=\"color: #33cccc;\">Q<\/span>.<span style=\"color: #ff0000;\">enqueue<\/span>(<span style=\"color: #33cccc;\">n\{<span id=\"PSid9\"><\/span>\}<\/span>)<\/p>"];
+
+var VarDict = {
+    vUpper:["vU",[[3,"PSid1"]]],
+   vLower:["vL",[[6,"PSid2"],[7,"PSid3"],[8,"PSid4"],[8,"PSid5"]]],
+   n:["n",[[8,"PSid6"],[9,"PSid7"],[10,"PSid8"],[11,"PSid9"]]],
+   dist:["",[[8,"PSid10"],[9,"PSid11"],[10,"PSid12"]]]
+   };
+/*
+var VarDict = {
+    n: [1,[[1,"id1"],[2,"id5"]]],
+    k: [2,[[3,"id9"],[6,"id4"],[5,"id8"]]]
+  };
+  
+  for(var key in VarDict) {
+    var value = VarDict[key];
+    console.log(value)
+    let result = VarDict[key][0];
+    console.log("result:"+result);
+    let inner = VarDict[key][1];
+    console.log(inner[0]);
+    for(let i = 0; i<inner.length;i++){
+      let lineNum = inner[i][0];
+      let lineID = inner[i][1];
+      
+      console.log("linenum:"+lineNum+"lineID:"+lineID);
+    }
+  }
+  */
+ //more advanced way to do variables
---
+++ js/pseudocode.js
@@ -0,0 +1,128 @@
+//global array holding the current psudocode text
+var CodeTextArr = [];
+var countA = 0;
+
+//stores which lines are displayed in the psudocode text
+var DisplayLines = [false,false,false,false,false,false,false,false,false,false,false,false];
+//stores which variable is on which line and which id
+
+function PushText(text) 
+{
+    CodeTextArr.push(text);
+}
+/*
+Causes the [a-b] lines to be displayed or not next time UpdateText is called, display can be set to true or false
+*/
+function DisplayRange2(a,b,isDisplayed)
+{
+    //Silently handle out of bounds errors by reseting to edge, a<=b is assumed
+    if(a < 0) a = 0;
+    if(b > DisplayLines.length) b = DisplayLines.length;
+
+    for(let i = a; i<=b;i+=1)
+    {
+        console.log("Displaying:"+i);
+        DisplayLines[i] = isDisplayed;
+    }
+    console.log(DisplayLines);
+}
+/*
+Causes the [a-b] lines to be displayed or not next time UpdateText is called, display can be set to true or false
+*/
+function DisplayRange(n)
+{
+    document.getElementById("codeList").innerHTML = "";
+    for(let i = 0; i<12;i+=1)
+    {
+        DisplayLines[i] = false;
+    }
+    //Silently handle out of bounds errors by reseting to edge, a<=b is assumed
+    for(let i = 0; i<=n;i+=1)
+    {
+        console.log("Displaying:"+i);
+        DisplayLines[i] = true;
+        document.getElementById("codeList").innerHTML += PseudocodeText[i];
+
+    }
+    UpdateVar();
+}
+/*
+Updates text displayed at the bottom of screen with the lines in PseudocodeText that have the same
+line number set to true on DisplayLines
+*/
+function UpdateText()
+{
+    //document.getElementById("codeList").innerHTML = textA.join("");
+    //reset html area to clear
+    document.getElementById("codeList").innerHTML = "";
+    for(let i = 0; i<PseudocodeText.length;i+=1)
+    {
+        if(DisplayLines[i] === true)
+        {
+            document.getElementById("codeList").innerHTML += PseudocodeText[i];
+        }
+    }
+    UpdateVar();
+}
+/*
+updates all the variables throughout the code
+*/
+function UpdateVar()
+{
+    for(var key in VarDict) {
+        console.log("Key:"+key);
+        let result = VarDict[key][0];
+        console.log("Result:"+result);
+        let inner = VarDict[key][1];
+        console.log("Inner:"+inner);
+        for(let i = 0; i<inner.length;i++){
+          let lineNum = inner[i][0];
+          let lineID = inner[i][1];
+          console.log("LineNo:"+lineNum+" LineID:"+lineID);
+          //if the line is being shown, update the relevent variable at that location
+          if(DisplayLines[lineNum])
+          {
+            console.log("DisplayLine:"+DisplayLines[lineNum]);
+            document.getElementById(lineID).innerHTML = result;
+            console.log("Updated variable:"+key+"at"+lineID+"with the value"+result);
+          }
+
+        }
+      }
+}
+/*
+Sets the variable with its given value
+vUpper,
+vLower,
+n
+*/
+function SetVar(varname,value)
+{
+    console.log("Updated "+varname+"with the value"+value);
+    VarDict[varname][0] = value;
+}
+function PopText()
+{
+    CodeTextArr.pop();
+}
+var counter = 0;
+function PushTest()
+{
+    var testInput = document.getElementById("testText").value;
+    SetVar("vUpper",testInput);
+    SetVar("vLower",testInput);
+    SetVar("n",testInput);
+    counter += 1;
+    DisplayRange(counter%12);
+
+}
+function PopTest()
+{
+    counter -= 2;
+    DisplayRange(counter%12);
+}
+function Test1()
+{
+    DisplayRange(0,11,true);
+    UpdateText();
+}
